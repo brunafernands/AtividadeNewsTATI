eadb9474089f5248d77d4f1f7aa5673b69b8aefc
Junior <juniorlemos15421@hotmail.com>
2019-05-24 10:51:32 -0300

    first commit

--- NewsTATI-eff934dcfeb3a7222afb72ec7b76047bc17b8d75/app/src/main/java/br/edu/ifpr/stiehl/todolist/ui/ResultAdapter.kt
+++ NewsTATI-eff934dcfeb3a7222afb72ec7b76047bc17b8d75/app/src/main/java/br/edu/ifpr/stiehl/todolist/ui/ResultAdapter.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.core.content.ContextCompat.startActivity
+import androidx.core.view.isInvisible
 import androidx.recyclerview.widget.RecyclerView
 import br.edu.ifpr.stiehl.todolist.R
 import br.edu.ifpr.stiehl.todolist.entidades.Article
@@ -16,6 +17,7 @@ import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_main.view.*
 import kotlinx.android.synthetic.main.item_article.*
 import kotlinx.android.synthetic.main.item_article.view.*
+import java.text.SimpleDateFormat
 
 class ResultAdapter(private var articles: List<Article>) :
     RecyclerView.Adapter<ResultAdapter.ResultViewHolder>() {
@@ -39,13 +41,31 @@ class ResultAdapter(private var articles: List<Article>) :
         fun preencherView(article:Article) {
             itemView.txtTitulo.setText(article.content)
             itemView.txtDescricao.setText(article.title)
+            itemView.txtAuthor.setText(article.author)
+
+            if(article.author.isNullOrBlank()){
+                itemView.txtAuthor.isInvisible
+            }
+            else{
+                itemView.txtAuthor.visibility
+            }
+
+            val inFormatter = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
+            val outFormatter = SimpleDateFormat("dd MMM yyyy, HH'h'mm")
+
+
+            val date = outFormatter.format(inFormatter.parse(article.publishedAt))
+            itemView.txtInforme.text = article.source.name + ", " + date
+
+
             itemView.VerMais.setOnClickListener {
                 val uri = Uri.parse(article.url)
                 val intent = Intent(Intent.ACTION_VIEW)
                 intent.setData(uri)
                 itemView.context.startActivity(intent)
             }
-            Picasso.get().load(article.urlToImage).into(itemView.imageArticle)
+            Picasso.get().load(article.urlToImage).placeholder(R.drawable.load).error(R.drawable.erro).into(itemView.imageArticle)
+
         }
 
     }
